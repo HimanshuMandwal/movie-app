f84531bc92ba4b1ffdc9e0133374b9347ec38966
himanshu mandwal <hkushwah8@gmail.com>
2020-11-22 22:58:47 +0530

    added the dispatch method to ADD_MOVIES actions

--- src/index.js
+++ src/index.js
@@ -4,9 +4,16 @@ import { createStore } from 'redux';
 import './index.css';
 import App from './components/App';
 import movies from './reducers/index';
-const store = createStore(movies);
- console.log(store);
 
+
+
+const store = createStore(movies);
+ console.log('before state ',store.getState());//used to get the state
+store.dispatch({ //dispatch method is used to send the actions
+  type : 'ADD_MOVIES',
+  movies : [{name :'mirzapur'}]
+});
+console.log('after state',store.getState());
 ReactDOM.render(
   <React.StrictMode>
     <App />
